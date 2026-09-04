18a5a0a3ba2a845a8cba79adeab54e2e7064025a
AaditMistry1114 <aaditmistry31@gmail.com>
2026-09-04 15:54:09 +0530

    feat: render products dynamically

--- shopping-cart/index.js
+++ shopping-cart/index.js
@@ -5,3 +5,37 @@ const products = [
     {id : 3, name:"Sofa", price : 35000, category : "Appliences"},
     {id : 4, name:"T-Shirt", price : 1000, category : "Clothes"}
 ];
+
+function displayProducts(){
+
+    const productsContainer = document.getElementById("products-container");
+
+    products.forEach((product) => {
+
+        // child element container
+        const productDiv = document.createElement("div");
+
+        // add each property of product by creating element and appending in child element.
+        const prodName = document.createElement("p");
+        prodName.textContent = product.name;
+
+        const prodPrice = document.createElement("p");
+        prodPrice.textContent = product.price;
+
+        const prodCategory = document.createElement("p");
+        prodCategory.textContent = product.category;
+
+        const addToCartBtn = document.createElement("button");
+        addToCartBtn.textContent = "Add to Cart";
+
+        productDiv.appendChild(prodName);
+        productDiv.appendChild(prodPrice);
+        productDiv.appendChild(prodCategory);
+        productDiv.appendChild(addToCartBtn);
+
+        // adding child container to parent container
+        productsContainer.appendChild(productDiv);
+    });
+}
+
+displayProducts();
