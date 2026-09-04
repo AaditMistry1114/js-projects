463df7dd51fd5a1b22a95bf670d18a2314f6b544
AaditMistry1114 <aaditmistry31@gmail.com>
2026-09-04 14:38:20 +0530

    feat: add products catalog data

---
+++ shopping-cart/index.js
@@ -0,0 +1,7 @@
+// contains all products 
+const products = [
+    {id : 1, name:"Laptop", price : 75000, category : "Electronics"},
+    {id : 2, name:"Book", price : 500, category : "Education"},
+    {id : 3, name:"Sofa", price : 35000, category : "Appliences"},
+    {id : 4, name:"T-Shirt", price : 1000, category : "Clothes"}
+];
