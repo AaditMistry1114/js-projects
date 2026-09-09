1b6dc0349736bf747a1fa8a4e3a131b4c487bd81
AaditMistry1114 <aaditmistry31@gmail.com>
2026-09-09 22:20:56 +0530

    feat: display detailed weather information

--- weather-app/index.js
+++ weather-app/index.js
@@ -20,6 +20,7 @@ async function getInformation() {
 
     result.textContent = "Loading.....";
     
+    // error handling for network issue etc etc 
     try {
 
         // Get latitude and longitude
@@ -43,12 +44,47 @@ async function getInformation() {
 
         // Get weather using coordinates
         const weatherinfo = await fetch(
-            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m`
+            `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code`
         );
 
         const weatherdata = await weatherinfo.json();
 
-        result.textContent = weatherdata.current.temperature_2m;
+        const weather_codes= {
+            0: "Clear sky",
+            1: "Mainly clear",
+            2: "Partly cloudy",
+            3: "Overcast",
+            45: "Fog",
+            48: "Depositing rime fog",
+            51: "Drizzle: Light intensity",
+            53: "Drizzle: Moderate intensity",
+            55: "Drizzle: Dense intensity",
+            56: "Freezing drizzle: Light intensity",
+            57: "Freezing drizzle: Dense intensity",
+            61: "Rain: Slight intensity",
+            63: "Rain: Moderate intensity",
+            65: "Rain: Heavy intensity",
+            66: "Freezing rain: Light intensity",
+            67: "Freezing rain: Heavy intensity",
+            71: "Snow fall: Slight intensity",
+            73: "Snow fall: Moderate intensity",
+            75: "Snow fall: Heavy intensity",
+            77: "Snow grains",
+            80: "Rain showers: Slight intensity",
+            81: "Rain showers: Moderate intensity",
+            82: "Rain showers: Violent",
+            85: "Snow showers: Slight",
+            86: "Snow showers: Heavy",
+            95: "Thunderstorm: Slight or moderate",
+            96: "Thunderstorm with slight hail",
+            99: "Thunderstorm with heavy hail"
+        }
+
+
+        result.innerHTML = `Temperature: ${weatherdata.current.temperature_2m} °C <br>
+                            Humidity: ${weatherdata.current.relative_humidity_2m} % <br>
+                            Wind Speed: ${weatherdata.current.wind_speed_10m} ${weatherdata.current_units.wind_speed_10m}<br>
+                            Weather Code: ${weather_codes[weatherdata.current.weather_code]}`;
 
     } catch (error) {
 
